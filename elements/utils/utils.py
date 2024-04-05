cd10416a11e1925beb2894990dd1ec0a6ab9e005
nathandxn <nathandxn@gmail.com>
2024-04-05 19:50:36 -0400

    Add new __init__.py

---
+++ elements/utils/utils.py
@@ -0,0 +1,31 @@
+from re import sub
+
+# some formatting helper functions
+def snake_case(text):
+    """This function is used for converting strings to snake_case."""
+
+    if text is None:
+        return None
+
+    else:
+        return "_".join(
+            sub(
+                "([A-Z][a=z]+)",
+                r" \1",
+                sub(
+                    "([A-Z]+)",
+                    r" \1",
+                    text.replace("-", " ")
+                    .replace("’", "")
+                    .replace("–", "")
+                    .replace("&", "and")
+                    .replace(",", "")
+                    .replace("/", " ")
+                    .replace(".", "")
+                    .replace("'", "")
+                    .replace(")", "")
+                    .replace("(", "")
+                    .replace(":", ""),
+                ),
+            ).split()
+        ).lower()
