4dfe5a72e41bba39075678a26c5cdfcdc7b7f5a4
nathandxn <nathandxn@gmail.com>
2024-04-05 20:20:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
+
 from setuptools import find_packages, setup
 
 with open("elements/README.md", "r") as f:
     long_description = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
     name="elements_utils",
     version="0.1.0",
     description="Utility functions and classes for data applications",
     package_dir={"": "elements"},
-    packages=find_packages(where="elements"),
+    packages=find_packages(exclude=('tests', 'docs')),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nathandxn/elements_utils",
     author="Nathan Dixon",
     author_email="nathandxn@gmail.com",
-    license="GNU v3.0",
+    license=license,
     classifiers=[
         "Programming Language :: Python :: 3.10",
     ],
