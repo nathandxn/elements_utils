93dabe6090d4058d25412b9df84a567781a5e0b3
nathandxn <nathandxn@gmail.com>
2024-04-05 21:47:26 -0400

    black format setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-
 from setuptools import find_packages, setup
 
 with open("elements/README.md", "r") as f:
     long_description = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
     name="elements",
     version="0.1.0",
     description="Utility functions and classes for data applications",
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=("tests", "docs")),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nathandxn/elements_utils",
